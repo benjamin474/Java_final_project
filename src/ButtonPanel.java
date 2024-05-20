e29eca4f47b118c5e24c1c482153abb6c6a658d8
rayhong1 <rayhonglol@gmail.com>
2024-05-20 17:53:37 +0800

    修改Button"確定要刪除嗎？"

--- src/ButtonPanel.java
+++ src/ButtonPanel.java
@@ -11,11 +11,14 @@ public class ButtonPanel extends JPanel {
         deleteButton = new JButton("刪除");
         finishButton = new JButton("已完成");
 
-        ButtonListener buttonListener = new ButtonListener();
-        detailsButton.addActionListener(buttonListener);
-        editButton.addActionListener(buttonListener);
-        deleteButton.addActionListener(buttonListener);
-        finishButton.addActionListener(buttonListener);
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int row = table.getSelectedRow();
+                if (row >= 0 && confirmAction("確定要刪除嗎？"))
+                    model.removeRow(row);
+            }
+        });
 
         finishButton.addActionListener(new ActionListener() {
             @Override
@@ -24,11 +27,9 @@ public class ButtonPanel extends JPanel {
                 if (row >= 0 && confirmAction("您已完成此計畫了嗎？")) {
                     System.out.println("完成計畫:" + row);
                     // 新增至 "已完成" List當中
-
                 }
             }
         });
-
 
         add(detailsButton);
         add(editButton);
@@ -36,42 +37,7 @@ public class ButtonPanel extends JPanel {
         add(finishButton);
     }
 
-
-//    public void actionPerformed(ActionEvent e){
-//        if(e.getSource() == detailsButton){
-//            int row = table.getSelectedRow();
-//            if (row >= 0 && confirmAction("確定要刪除嗎？"))
-//                model.removeRow(row);
-//        }
-//        if(e.getSource() == deleteButton){
-//
-//        }
-//    }
-
     private boolean confirmAction(String text) {
         return JOptionPane.showConfirmDialog(this, text, "確認", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
     }
-
-    private class ButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-//            if (e.getSource() == deleteButton) {
-//                int row = table.getSelectedRow();
-//                System.out.println("Delete Button");
-//                if (row >= 0 && confirmAction("確定要刪除嗎？")) {
-//                    model.removeRow(row);
-//                }
-//            }
-            if (e.getSource() == editButton) {
-                System.out.println("EditButton");
-            }
-            if (e.getSource() == detailsButton) {
-                System.out.println("DetailsButton");
-            }
-            if (e.getSource() == finishButton) {
-                System.out.println("FinishButton");
-            }
-        }
-    }
-
-
 }
